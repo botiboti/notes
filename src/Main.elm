603d1dd1dd33fe5a5097489346b21d8f74f2338c
botiboti <botondferenczi98@gmail.com>
2019-04-24 20:30:39 +0300

    mvp

---
+++ src/Main.elm
@@ -0,0 +1,29 @@
+import Browser exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+type alias Model = List String
+
+type Msg =
+  Add String
+
+init : Model
+init =
+  []
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Add text ->
+      model ++ (List.singleton <| text)
+
+view : Model -> Html Msg
+view model =
+  div []
+     [
+     div []  (List.map (\txt -> text txt) model)
+     ]
