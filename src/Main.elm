03d2806708d306a016994d12c0c62acc1d1f8da1
botiboti <botondferenczi98@gmail.com>
2019-04-27 13:04:50 +0300

    edit note | delete note

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,8 @@ import Browser exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import List exposing (..)
+import List.Extra as LE
 
 main =
   Browser.sandbox { init = init, update = update, view = view }
@@ -9,7 +11,9 @@ main =
 type alias Model = List String
 
 type Msg =
-  Add String
+     Add String
+  |  Edit String String
+  |  Delete String
 
 init : Model
 init =
@@ -18,12 +22,37 @@ init =
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Add text ->
-      model ++  [text]
+    Add note ->
+      model ++  [note]
+
+    Edit old new ->
+      LE.updateIf (\note -> note == old) (\note -> new) model
+
+    Delete note ->
+      LE.remove note model
+
+{--
+editNote : String -> String -> Model -> Model
+editNote old new model =
+  LE.updateIf (\note -> note == old) (\note -> new) model
+-}
 
 view : Model -> Html Msg
 view model =
   div []
-     [
-     div []  (List.map (\txt -> text txt) model)
+     [ div []  (List.map (\txt -> text txt) (update (Edit "kutya" "cica") model))
+     , tests
      ]
+
+tests : Html Msg
+tests =
+  let
+    ok = div [ style "color" "green" ] [ text "OK" ]
+    err = div [ style "color" "red" ] [ text "Eh.." ]
+    model = ["hello", "kutya", "haha"]
+  in
+      [ update (Add "p") model == ["hello", "kutya", "haha", "p"]
+      , update (Edit "kutya" "cica") model == ["hello", "cica", "haha"]
+      , update (Delete "haha") model == ["hello", "kutya"]]
+        |> List.map (\bool -> if bool then ok else err)
+        |> div []
